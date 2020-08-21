391936e15470945c9741f872cef540c19de0e091
desyfajri <70006529+desyfajri@users.noreply.github.com>
2020-08-21 14:28:09 +0700

    Add files via upload

---
+++ vscode.php
@@ -0,0 +1,19 @@
+<?php
+
+    //contoh class
+    class makanan{
+
+
+        // contoh dari properti 
+        public $enak;
+        public $mantep;
+
+        // contoh dari method
+        function mengenyangkan{
+            return "bersendawa"
+        }
+    }
+    //contoh membuat object dari class "makanan"
+    $jazz = new makanan ();
+
+    ?>
